c49a27ceed28c423b8bc1badc79e7806a5eb9377
Orion(Mac) <qiannianmen_110@sina.com>
2016-02-28 15:55:16 +0800

    为了能打出.so所做的修改

--- android-gen.sh
+++ android-gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#这里的环境变量设置根本不起作用，需要在使用的时候设置
 export ANDROID_TOOLCHAIN_DIR=$PWD/android-toolchain
 export PATH=$ANDROID_TOOLCHAIN_DIR/bin:$PATH
 export AR=arm-linux-androideabi-ar
@@ -8,4 +9,5 @@ export CXX=arm-linux-androideabi-g++
 export LINK=arm-linux-androideabi-g++
 export PLATFORM=android
 
-gyp --depth=. -Dtarget_arch=arm -DOS=android -Dpomelo_library=static_library -Duse_sys_openssl=false -Dbuild_jpomelo=true pomelo.gyp --format=make
+gyp --depth=. -Dtarget_arch=arm -DOS=android -Dpomelo_library=static_library -Duse_sys_openssl=false -Dbuild_jpomelo=true pomelo.gyp --format=make-android
+
